1498d65ca72c647dc940b29d9c2acdc671a822eb
filipe resende <filipee.resende@gmail.com>
2022-06-14 21:36:21 -0300

    Alterado de string para stringbuilder. Responsabilidades de métodos separadas.

---
+++ DTOs/ConfiguracoesEmailResponse.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Net.Mail;
+
+namespace EnviaEmailSmtp.DTOs
+{
+    public class ConfiguracoesEmailResponse
+    {
+        public MailMessage Mail { get; set; }
+        public IConfiguration Configuration { get; set; }
+    }
+}
